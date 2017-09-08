841111eb534a6c0a72209399682540f45e645e8e
Sterling <sterlingmd17@gmail.com>
2017-09-07 17:39:15 -0700

    worked on TODO 1,2

--- src/main/java/org/launchcode/controllers/JobController.java
+++ src/main/java/org/launchcode/controllers/JobController.java
@@ -1,12 +1,15 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.Job;
 import org.launchcode.models.forms.JobForm;
 import org.launchcode.models.data.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
 
@@ -20,10 +23,12 @@ public class JobController {
     private JobData jobData = JobData.getInstance();
 
     // The detail display for a given Job at URLs like /job?id=17
-    @RequestMapping(value = "", method = RequestMethod.GET)
-    public String index(Model model, int id) {
+    @RequestMapping(value = "/job?id={id}", method = RequestMethod.GET)
+    public String index(Model model, @PathVariable("id") int id) {
 
         // TODO #1 - get the Job with the given ID and pass it into the view
+        Job theJob=jobData.findById(id);
+        model.addAttribute("jobs",theJob);
 
         return "job-detail";
     }
