bea3bd85989abfe7750be56e6b7ec74741677590
Sterling <sterlingmd17@gmail.com>
2017-09-07 20:46:09 -0700

    finished up to do 6

--- src/main/java/org/launchcode/controllers/JobController.java
+++ src/main/java/org/launchcode/controllers/JobController.java
@@ -1,5 +1,6 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.Employer;
 import org.launchcode.models.Job;
 import org.launchcode.models.forms.JobForm;
 import org.launchcode.models.data.JobData;
@@ -23,8 +24,8 @@ public class JobController {
     private JobData jobData = JobData.getInstance();
 
     // The detail display for a given Job at URLs like /job?id=17
-    @RequestMapping(value = "/job?id={id}", method = RequestMethod.GET)
-    public String index(Model model, @PathVariable("id") int id) {
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String index(Model model, int id) {
 
         // TODO #1 - get the Job with the given ID and pass it into the view
         Job theJob=jobData.findById(id);
@@ -42,11 +43,18 @@ public class JobController {
     @RequestMapping(value = "add", method = RequestMethod.POST)
     public String add(Model model, @Valid JobForm jobForm, Errors errors) {
 
+        if (errors.hasErrors()){
+            return"redirect: add";
+        }
+
+        Employer anEmployer = jobData.getEmployers().findById(jobForm.getEmployerId());
+
+
         // TODO #6 - Validate the JobForm model, and if valid, create a
         // new Job and add it to the jobData data store. Then
         // redirect to the job detail view for the new Job.
 
-        return "";
+        return "redirect ";
 
     }
 }
--- src/main/java/org/launchcode/models/forms/JobForm.java
+++ src/main/java/org/launchcode/models/forms/JobForm.java
@@ -21,12 +21,16 @@ public class JobForm {
 
     @NotNull
     private int employerId;
+    private int locationId;
+    private int coreCompetencyId;
+    private int positionTypeId;
 
     /*
         TODO #3 - Included other fields needed to create a job,
         with correct validation attributes and display names.
         Don't forget to add getters and setters
      */
+
 
     private ArrayList<Employer> employers;
     private ArrayList<Location> locations;
@@ -42,7 +46,34 @@ public class JobForm {
         */
 
         employers = jobData.getEmployers().findAll();
+        locations= jobData.getLocations().findAll();
+        coreCompetencies= jobData.getCoreCompetencies().findAll();
+        positionTypes = jobData.getPositionTypes().findAll();
 
+    }
+
+    public int getLocationId() {
+        return locationId;
+    }
+
+    public void setLocationId(int locationId) {
+        this.locationId = locationId;
+    }
+
+    public int getCoreCompetencyId() {
+        return coreCompetencyId;
+    }
+
+    public void setCoreCompetencyId(int coreCompetencyId) {
+        this.coreCompetencyId = coreCompetencyId;
+    }
+
+    public int getPositionTypeId() {
+        return positionTypeId;
+    }
+
+    public void setPositionTypeId(int positionTypeId) {
+        this.positionTypeId = positionTypeId;
     }
 
     public String getName() {
